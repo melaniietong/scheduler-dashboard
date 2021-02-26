c7b9ae75f59f6cb2fa15db657e31b06597b02a74
Melanie Tong <melanie@Melanies-MacBook-Air.local>
2021-02-25 23:12:15 -0800

    Add Promise to componentDidMount() function

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import classnames from "classnames";
 import Loading from "components/Loading";
@@ -29,9 +30,12 @@ const data = [
 
 class Dashboard extends Component {
   state = {
-    loading: false,
-    focused: null
-  };
+    loading: true,
+    focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {}
+   };
 
   selectPanel(id) {
     this.setState(previousState => ({
@@ -45,6 +49,19 @@ class Dashboard extends Component {
     if (focused) {
       this.setState({ focused });
     }
+
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data
+      });
+    });
   }
 
   componentDidUpdate(previousProps, previousState) {
